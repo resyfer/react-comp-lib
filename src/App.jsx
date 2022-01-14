5ecd6dbccac3e40a37fe30221f3a825e70dc3ec1
resyfer <resyfer.dev@gmail.com>
2022-01-14 14:47:45 +0530

    base setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5EDCE5",
+        secondary: "#434343",
+        tertiary: "#6C8DAE",
+        back: "#F5F9FF",
+      },
+    },
+  },
+  plugins: [],
+};
